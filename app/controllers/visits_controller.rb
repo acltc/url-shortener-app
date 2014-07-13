fbb0739fbb8230fef1b42fb8f291e97629e41c89
Jay Wengrow <jaywngrw@gmail.com>
2014-07-12 22:13:32 -0500

    handle when slug is not found in database

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,8 +2,11 @@ class VisitsController < ApplicationController
   def create
     @link = Link.find_by(:slug => params[:slug])
 
-    @link.visits.create(:ip_address => request.remote_ip)
-
-    redirect_to "https://#{@link.target_url}"
+    if @link
+      @link.visits.create(:ip_address => request.remote_ip)
+      redirect_to "https://#{@link.target_url}"
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 end
