f4afe975ba80c0630a63f5419c70d6362b2c3378
Jay Wengrow <jaywngrw@gmail.com>
2014-07-12 22:23:23 -0500

    edit and update links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -23,4 +23,19 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    if @link.update(params[:link])
+      @link.standardize_target_url!
+      flash[:success] = "Link created successfully"
+      redirect_to links_path
+    else
+      render 'new'
+    end
+  end
+
 end
